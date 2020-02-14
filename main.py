80fc05d5706e9bf7caf1818dab78e937913d3af8
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-02-14 15:49:57 +0100

    building a skeleton of the first list of commands

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 print("Welcome to the Git List Retriever Tool")
-print("Please, select one of the following commands:")
+help_message = '''Please, select one of the following commands
+GO -> starts the program
+SETTINGS -> change some option
+QUIT -> quit the program'''
+print(help_message)
+
+while True:
+    command = input("> ").lower()
+    if command == 'help':
+        print(help_message)
+    elif command == 'go':
+        print('starting....')
+    elif command == 'settings':
+        print('going to settings')
+    elif command == 'quit':
+        break
+    else:
+        print(f"Command not found!\n{help_message}")
