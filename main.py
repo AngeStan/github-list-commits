7293fd4e460262128490968c12990fe8954e62f8
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-02-15 12:13:11 +0100

    improved help message structure

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ paths = {'GO': 'start the program', 'SETTINGS': 'change some option', 'QUIT': 'q
 
 
 def help_message():
-    print('\nPlease, select one of the following commands:')
+    print('Please, select one of the following commands:')
     for key in paths:
         print(key, ' -> ', paths[key])
 
 
+print('This program lets you retrieve a number (of your choice) \
+of commits from a selected repo.\n')
 help_message()
 
 while True:
     command = input("> ").lower()
-    if command == 'help':
-        help_message()
-    elif command == 'go':
+    if command == 'go':
         print('starting....')
     elif command == 'settings':
         print('going to settings')
