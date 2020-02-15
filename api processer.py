174a06fb9c78353c1035fe09fed289091078a9d4
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-02-15 22:37:09 +0100

    fix: the code can now successfully get all the chosen commits number, looping correctly all the necessary pages

--- api processer.py
+++ api processer.py
@@ -1,4 +1,5 @@
 import urllib.request, urllib.error, json
+
 '''
 with open('settings.json', 'r') as settings_file:
     json_file = json.loads(settings_file)
@@ -6,13 +7,23 @@ repo = json_file['repo']'''
 # debug-----------
 repo = 'freeCodeCamp/freeCodeCamp'
 url_api = 'https://api.github.com/repos/' + repo + '/commits?branch=master'
-remaining_commits = 32
-sha = None
+number_commits = remaining_commits = 69
+if remaining_commits > 30:
+    page_commits = 30
+else:
+    page_commits = remaining_commits
+sha = starter = 0
 table = []
 
 while remaining_commits > 0:
-    if sha is not None:
+    if remaining_commits > 30:
+        page_commits = 29
+    else:
+        page_commits = remaining_commits
+
+    if sha != 0:
         url_api = url_api.split('&')[0] + '&sha=' + str(sha)
+        starter = 1
     try:
         json_received = urllib.request.urlopen(url_api)
     except urllib.error.HTTPError as error:
@@ -26,15 +37,14 @@ while remaining_commits > 0:
         # processing...
         commits = json.loads(json_received.read().decode())
 
-    for commit in commits:
-        sha = commit['sha']
-        message = commit['commit']['message']
-        url = commit['html_url']
+    print(page_commits)
+    for i in range(starter, page_commits + 1):
+        sha = commits[i]['sha']
+        message = commits[i]['commit']['message']
+        url = commits[i]['html_url']
         table.append({'sha': sha, 'message': message, 'url': url})
         remaining_commits -= 1
-
-
-
+        print(remaining_commits)
 
 print(table)
 print(len(table))
