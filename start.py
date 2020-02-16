3e925b32a2445790726e2da67f4b05a0b5ecaae1
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-02-16 11:12:37 +0100

    fixup! new: recreated the main module "start.py" improved: "start.py" can import and work with "api_processor.py"

---
+++ start.py
@@ -0,0 +1,17 @@
+from features.api_processor import *
+repo = 'freeCodeCamp/freeCodeCamp'
+
+print(f'''Welcome to the Git List Retriever Tool\n\
+This program lets you retrieve a number (of your choice) \
+of commits from a selected repo.\nThe default repository is "{repo} in branch "master"''')
+print('Please, insert a number of repositories you want to retrieve')
+
+while True:
+    try:
+        number = int(input("> "))
+    except ValueError as error:
+        print('You have inserted a number! Please try again')
+    finally:
+        break
+
+print(process_api(repo, number))
