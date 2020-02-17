cd9ca87f32610f77c08f7eebd4cf2d73da0b2da6
AngeStan <angestan@protonmail.ch>
2020-02-17 16:46:53 +0100

    changed: moved function connect(api) for a clearer code

--- features/api_processor.py
+++ features/api_processor.py
@@ -1,31 +1,31 @@
 import urllib.request, urllib.error, json
 
 
+def connect(url):
+    try:
+        json_received = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as error:
+        # Return code error (e.g. 404, 501, ...)
+        print('HTTPError: ', error.code)
+    except urllib.error.URLError as error:
+        # Not an HTTP-specific error (e.g. connection refused)
+        print('URLError: ', error.reason)
+    finally:
+        print('Connected Successfully!')
+        json_data = json.loads(json_received.read().decode())
+        return json_data
+
+
 def process_api(author_repo, number_commits):
     url_api = 'https://api.github.com/repos/' + author_repo + '/commits?branch=master'
     last_sha = starter = 0
     table = []
 
-    def connect(url):
-        try:
-            json_received = urllib.request.urlopen(url)
-        except urllib.error.HTTPError as error:
-            # Return code error (e.g. 404, 501, ...)
-            print('HTTPError: ', error.code)
-        except urllib.error.URLError as error:
-            # Not an HTTP-specific error (e.g. connection refused)
-            print('URLError: ', error.reason)
-        finally:
-            print('Connected Successfully!')
-            # processing...
-            # json_received = urllib.request.urlopen(url)
-            json_data = json.loads(json_received.read().decode())
-            return json_data
-
     while number_commits > 0:
         if last_sha != 0:
             url_api = url_api.split('&')[0] + '&sha=' + str(last_sha)
             starter = 1
+
         commits = connect(url_api)
 
         for i in range(starter, 30):
@@ -36,7 +36,7 @@ def process_api(author_repo, number_commits):
             if number_commits == 0:
                 break
 
-    # Texts in "Message" column adjustments
+    # Adjustments of the texts in "Message"
     for row in range(len(table)):
         table[row][1] = table[row][1].replace("\n", "")  # removes windows:[newline] chars
         table[row][1] = table[row][1].replace("\r", "")  # removes mac:[newline] chars
