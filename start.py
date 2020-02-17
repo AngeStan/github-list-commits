db2ca22599d6bc0a11f010d84019dedd1e675841
AngeStan <angestan@protonmail.ch>
2020-02-17 16:23:05 +0100

    new: added possibility to run it again

--- start.py
+++ start.py
@@ -12,22 +12,24 @@ of commits from a selected repo.\nThe default repository is "{author}/{repo}" in
 text_commits_count, commits_count = parse_count_commits(main_url)
 
 print(f'Please, insert a number of repositories you want to retrieve (max: {text_commits_count})')
-
 while True:
-    try:
-        number = int(input("> "))
-    except ValueError as error:
-        print('You have NOT inserted a number! Please try again')
-        continue
-    finally:
-        if number <= 0:
-            print("Please choose at least 1 commit to get.")
+    while True:
+        try:
+            number = int(input("> "))
+        except ValueError as error:
+            print('You have NOT inserted a number!')
             continue
-        elif number > commits_count:
-            print(f"'{number}' is too much! Total of available commits to retrieve is '{text_commits_count}'")
-            continue
-        break
+        finally:
+            if number <= 0:
+                print("Please choose at least 1 commit to get.")
+                continue
+            elif number > commits_count:
+                print(f"'{number}' is too much! Total of available commits to retrieve is '{text_commits_count}'")
+                continue
+            break
 
-table = process_api(f'{author}/{repo}', number)
-export_csv(table)
-export_db(table)
+    table = process_api(f'{author}/{repo}', number)
+    export_csv(table)
+    export_db(table)
+    print("\nThank you for using this tool. If you want to run it again, please enter another number.")
+    number = 0
